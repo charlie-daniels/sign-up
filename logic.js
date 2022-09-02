9ff46a3a81e1147814392b89746eca210ad6c723
charlie-daniels <charlied030401@gmail.com>
2022-09-02 14:48:54 +0100

    Add password confirm

---
+++ logic.js
@@ -0,0 +1,18 @@
+function checkPasswords(a, b) {
+    let firstContainer = document.querySelector('li.password');
+    if (a.value !== b.value) {
+        a.setCustomValidity('Passwords do not match.');
+        b.setCustomValidity('Passwords do not match.');
+        firstContainer.classList.add('password-invalid');
+    } else {
+        a.setCustomValidity('');
+        b.setCustomValidity('');
+        firstContainer.classList.add('password-invalid');
+    }
+}
+
+passwords = document.querySelectorAll('input[type="password"]');
+
+passwords[1].addEventListener('input', () => {
+    checkPasswords(passwords[0], passwords[1]);
+});
